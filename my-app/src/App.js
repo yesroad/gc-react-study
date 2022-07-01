72f2ca9ed8cc365546f1543731bdaf4140029576
김응길 <yesroad.dev@gmail.com>
2022-07-01 17:12:03 +0900

    기본 투두리스트 작성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [value, setValue] = useState('')
+  const [list, setList] = useState([]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(!value){
+      return alert('내용을 입력하세요');
+    }
+    setList([...list, value.trim()])
+    setValue('')
+  }
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={value} onChange={onChange}/>
+        <button>등록</button>
+      </form>
+      <ul>
+        {
+          list.map((item ,index) =>(
+              <li key={index}>{item}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
