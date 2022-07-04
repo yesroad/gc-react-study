020bc6cc53f7f64240f2131a97e7f72b25fa46f9
김응길 <yesroad.dev@gmail.com>
2022-07-04 15:58:46 +0900

    토글 기능 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,61 @@
-import {useState} from "react";
+import { useState } from 'react';
 
 function App() {
-  const [value, setValue] = useState('')
-  const [list, setList] = useState([]);
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if(!value){
-      return alert('내용을 입력하세요');
-    }
-    setList([...list, value.trim()])
-    setValue('')
-  }
+	const onSubmit = (e) => {
+		e.preventDefault();
 
-  const onChange = (e) => {
-    setValue(e.target.value);
-  }
+		if (value.trim() === '') {
+			setValue('');
+			return alert('내용을 입력하세요');
+		}
 
-  return (
-    <div className="App">
-      <form onSubmit={onSubmit}>
-        <input type="text" value={value} onChange={onChange}/>
-        <button>등록</button>
-      </form>
-      <ul>
-        {
-          list.map((item ,index) =>(
-              <li key={index}>{item}</li>
-          ))
-        }
-      </ul>
-    </div>
-  );
+		setList([
+			...list,
+			{
+				value: value.trim(),
+				isChecked: false,
+			},
+		]);
+
+		setValue('');
+	};
+
+	const onChange = (e) => {
+		setValue(e.target.value);
+	};
+
+	const onClick = (id) => {
+		setList(
+			list.map((item, index) =>
+				id === index ? { ...item, isChecked: !item.isChecked } : item,
+			),
+		);
+	};
+
+	return (
+		<div className='App'>
+			<form onSubmit={onSubmit}>
+				<input type='text' value={value} onChange={onChange} />
+				<button>등록</button>
+			</form>
+			<ul>
+				{list.map((item, index) => (
+					<li
+						key={index}
+						onClick={() => onClick(index)}
+						style={{
+							textDecoration: `${item.isChecked ? 'line-through' : 'none'}`,
+						}}
+					>
+						{item.value}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
