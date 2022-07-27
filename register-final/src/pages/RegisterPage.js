53961c9bd70e56c7248ecd2f096255b827edaa49
김응길 <yesroad.dev@gmail.com>
2022-07-27 22:26:40 +0900

    약관동의 안했을시 약관 페이지로 이동

--- register-final/src/pages/RegisterPage.js
+++ register-final/src/pages/RegisterPage.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Register } from '../components/domain';
+import { AuthContext } from '../contexts/AuthProvider';
 
 function RegisterPage() {
+	const navigater = useNavigate();
+	const { state } = AuthContext();
+
+	useEffect(() => {
+		if(!state.terms.termsService || !state.terms.termsPrivacy) {
+			alert('약관동의부터 하고오세요.')
+			return navigater('/')
+		}
+	}, [navigater, state])
+	
 	return (
 		<>
 			<Register />
